92afbdacc64707b34e3ef54828b9e9feb91e4d50
GamerDTK <dekeji1@gmail.com>
2024-04-18 00:33:00 +0100

    hero section basically done, responsive sha

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -27,47 +27,52 @@ const Home = ({ scrolled }) => {
 
         <div className="container-fluid">
           <div className="content">
-            <h1>
-              Welcome To
-              Soteria Church
-            </h1>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur
-              adipiscing elit. Fusce malesuada in nunc a
-              placerat. In scelerisque eros et ipsum gravida
-              porta. Nullam volutpat eros a tempus
-              efficitur.
-            </p>
+            <div className="text">
+              <h1>
+                Welcome To
+                Soteria Church
+              </h1>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur
+                adipiscing elit. Fusce malesuada in nunc a
+                placerat. In scelerisque eros et ipsum gravida
+                porta. Nullam volutpat eros a tempus
+                efficitur.
+              </p>
+            </div>
+
 
             <div className="buttons">
               <Link to="/company" className='page-btn'>What's Next <ArrowForwardIcon fontSize='21' className='icon' /></Link>
             </div>
           </div>
-          <div className="event-countdown-card">
-            <OptimizedImage src="/assets/sermon.jpg" className="img" alt="event-countdown-card-img" blurhash="LcLEHB~pIUIU_4xvt7j@E2NHRjof" />
-            <div className="details">
-              <h5 className='title'>Upcoming Event With A Title</h5>
-              <div className="item">
-                <TodayIcon className="icon" />
-                <span>Monday 20th - Teusday 21 April 2021</span>
+          <div className="event-countdown-card-wrapper">
+            <div className="event-countdown-card">
+              <OptimizedImage src="/assets/sermon.jpg" className="img" alt="event-countdown-card-img" blurhash="LcLEHB~pIUIU_4xvt7j@E2NHRjof" />
+              <div className="details">
+                <h5 className='title'>Upcoming Event With A Title</h5>
+                <div className="item">
+                  <TodayIcon className="icon" />
+                  <span>Monday 20th - Teusday 21 April 2021</span>
+                </div>
+                <div className="item">
+                  <LocationOnOutlinedIcon className="icon" />
+                  <span>Woodlawn, Maryland, Baltimore, USA.</span>
+                </div>
+                <div className="register"><Link>Register</Link><KeyboardArrowRightOutlinedIcon className='icon' /> </div>
               </div>
-              <div className="item">
-                <LocationOnOutlinedIcon className="icon" />
-                <span>Woodlawn, Maryland, Baltimore, USA.</span>
+              <div className="countdown">
+                <Timer targetDateStr={"2024-04-19T20:48:00"} />
               </div>
-              <div className="register"><Link>Register</Link><KeyboardArrowRightOutlinedIcon className='icon' /> </div>
-            </div>
-            <div className="countdown">
-              <Timer targetDateStr={"2024-04-19T20:48:00"} />
             </div>
           </div>
+
         </div>
 
 
 
-        <div className="dotted"></div>
-        <OptimizedImage src="/assets/preaching.jpg" className="hero-img" alt="hero-img" blurhash="LcLEHB~pIUIU_4xvt7j@E2NHRjof" />
-        {/* <img src={require("../imgs/quantum-ex-capital-solutions/finance.jpg")} className="hero-img" alt="hero-img" backgroundImage/> */}
+        <OptimizedImage src="/assets/preaching.jpg" className="hero-img" wrapperClassName="hero-img-wrapper" alt="hero-img" blurhash="LcLEHB~pIUIU_4xvt7j@E2NHRjof" />
+        {/* <img src="/assets/preaching.jpg" className="hero-img" alt="hero-img" /> */}
 
       </section>
 
